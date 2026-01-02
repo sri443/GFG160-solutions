1d300b3fe469be2df42e0fb3c23578b24f704145
sri443 <raksdot223@gmail.com>
2026-01-02 11:37:51 +0530

    Update Day 67 - Merge two sorted linked list.c

--- solutions in c/Linked List/Day 67 - Merge two sorted linked list.c
+++ solutions in c/Linked List/Day 67 - Merge two sorted linked list.c
@@ -1,1 +1,27 @@
-
+struct Node* sortedMerge(struct Node* head1, struct Node* head2) {
+    struct Node* dummy=(struct Node*)malloc(sizeof(struct Node));
+    dummy->data=0;
+    dummy->next=NULL;
+    
+    struct Node* curr=dummy;
+    
+    while(head1!=NULL && head2!=NULL){
+        if(head1->data < head2->data){
+            curr->next=head1;
+            head1=head1->next;
+        }
+        else{
+            curr->next=head2;
+            head2=head2->next;
+        }
+        curr=curr->next;
+    }
+    
+    if(head1!=NULL){
+        curr->next=head1;
+    }else if(head2!=NULL){
+        curr->next=head2;
+    }
+    
+    return dummy->next;
+}
