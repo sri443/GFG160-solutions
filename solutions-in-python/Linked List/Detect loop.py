1764deebdc29f06f83ce81ff4df7598cb7bce7f5
sri443 <raksdot223@gmail.com>
2026-01-06 12:07:41 +0530

    Create Detect loop.py

---
+++ solutions-in-python/Linked List/Detect loop.py
@@ -0,0 +1,12 @@
+def detectLoop(self, head):
+        if head is None or head.next is None:
+            return False
+        
+        fast=head
+        slow=head
+        while fast is not None and fast.next is not None:
+            fast=fast.next.next
+            slow=slow.next
+            if fast==slow:
+                return True
+        return False
